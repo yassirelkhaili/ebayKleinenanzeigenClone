4418ceecdaa49a2c416b920b84148d2c7a4f66fb
yassirelkhaili <elkhailiyassir@gmail.com>
2023-02-02 20:24:02 +0100

    front page production ready

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -37,6 +37,9 @@ const Main = () => {
     </div>
     <div className=" main-container container-fluid row">
       <div className="accordion categories col-2" id="accordionPanelsStayOpenExample">
+      <div className="accordion-button" type="button" id='header'>
+      Beliebte Kategorien:
+      </div>
   <div className="accordion-item">
     <h2 className="accordion-header" id="panelsStayOpen-headingOne">
       <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
